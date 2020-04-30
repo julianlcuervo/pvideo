ab5d0cd5e2e7aa0d888300aeab2f259181b5981d
julian <julianccuervo@gmail.com>
2020-04-30 03:31:56 +0000

    Cambios en el login

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -1,36 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/styles/components/LoginBox.scss';
 import { Link } from 'react-router-dom'
 
-const LoginBox = (props) => {
+class LoginBox extends Component {
 
-    function useRegister() {
+    constructor(props) {
+        super(props);
+        this.useRegister = this.useRegister.bind(this);
+    }
+
+    useRegister() {
         let email = document.getElementById("email");
         let password = document.getElementById("password")
         if (email && password) {
             if (email.value !== "" && password.value !== "") {
-                props.useLogin([email.value, password.value])
+                this.props.useLogin([email.value, password.value])
             }
         }
     }
 
-    return (
-        <section className="login__container">
-            <h2>Inicia sesión</h2>
-            <form className="login__container--form">
-                <input id="email" type="search" className="input" placeholder="Correo" />
-                <input id="password" className="input" type="password" placeholder="Contraseña" />
-                <input id="buttonRegister" onClick={useRegister} type="button" className="comment-button" value="Ingresar" />
-            </form>
+    render() {
+        return (
+            <section className="login__container">
+                <h2>Inicia sesión</h2>
+                <form className="login__container--form">
+                    <input id="email" type="search" className="input" placeholder="Correo" />
+                    <input id="password" className="input" type="password" placeholder="Contraseña" />
+                    <input id="buttonRegister" onClick={this.useRegister} type="button" className="comment-button" value="Ingresar" />
+                </form>
 
-            <p className="login__container--register">
-                No tienes ninguna cuenta
+                <p className="login__container--register">
+                    No tienes ninguna cuenta
                 <Link to="/register">
-                    Regístrate
+                        Regístrate
                 </Link>
-            </p>
-        </section>
-    )
+                </p>
+            </section>
+        )
+    }
 }
 
 export default LoginBox;
