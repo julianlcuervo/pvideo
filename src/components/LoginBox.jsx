4612a08be27ab55596af0b5e68ecf307805a9991
unknown <andreamunos1999@gmail.com>
2020-04-29 22:46:45 -0500

    Se agregan validaciones para inicio de sesión

--- src/components/LoginBox.jsx
+++ src/components/LoginBox.jsx
@@ -7,8 +7,38 @@ class LoginBox extends Component {
     constructor(props) {
         super(props);
         this.useRegister = this.useRegister.bind(this);
+        this.state = {
+            fields: {},
+            errors: {}
+          }
+    
     }
+    handleValidation() {
+        let fields = this.state.fields;
+        let errors = {};
+        let formIsValid = true;
 
+    //Validaciones Email
+    if (!fields["email"]) {
+        formIsValid = false;
+        errors["email"] = "Campo obligatorio, no puede estar vacio";
+      }
+      if (typeof fields["email"] !== "undefined") {
+        let lastAtPos = fields["email"].lastIndexOf('@');
+        let lastDotPos = fields["email"].lastIndexOf('.');
+        if (!(lastAtPos < lastDotPos && lastAtPos > 0 && fields["email"].indexOf('@@') == -1 && lastDotPos > 2 && (fields["email"].length - lastDotPos) > 2)) {
+          formIsValid = false;
+          errors["email"] = "Correo inválido";
+        }
+      }
+      //Validar campo contraseña
+      if (!fields["password"]) {
+        formIsValid = false;
+        errors["password"] = "Campo obligatorio, no puede estar vacio";
+      }
+      this.setState({ errors: errors });
+      return formIsValid;
+    }
     useRegister() {
         let email = document.getElementById("email");
         let password = document.getElementById("password")
@@ -18,17 +48,39 @@ class LoginBox extends Component {
             }
         }
     }
+    contactSubmit(e) {
+        e.preventDefault();
+        if (this.handleValidation()) {
+          alert("Inicio de Sesión exitoso");
+        } else {
+          alert("Errores en el formulario")
+        }
+      }
+    
+      handleChange(field, e) {
+        let fields = this.state.fields;
+        fields[field] = e.target.value;
+        this.setState({ fields });
+      }
+    
 
     render() {
         return (
             <section className="login__container">
                 <h2>Inicia sesión</h2>
-                <form className="login__container--form">
-                    <input id="email" type="search" className="input" placeholder="Correo" />
-                    <input id="password" className="input" type="password" placeholder="Contraseña" />
-                    <input id="buttonRegister" onClick={this.useRegister} type="button" className="comment-button" value="Ingresar" />
+                <form className="login__container--form" onSubmit={this.contactSubmit.bind(this)}>
+                    <input id="email" type="search" className="input" placeholder="Correo"  onChange={this.handleChange.bind(this, "email")} value={this.state.fields["email"]} />
+                    <br></br>
+                    <h6 style={{ color: "white" }}>{this.state.errors["email"]}</h6>
+                    <input id="password" className="input" type="password" placeholder="Contraseña" onChange={this.handleChange.bind(this, "password")} value={this.state.fields["password"]} />
+                    <br></br>
+                    <h6 style={{ color: "white" }}>{this.state.errors["password"]}</h6>
+                    <div className="col-md-12">
+                        <button className="comment-button" id="buttonRegister" value="Ingresar" onClick={this.useRegister}>Iniciar sesión</button>
+                    </div>  
                 </form>
-
+                   
+          
                 <p className="login__container--register">
                     No tienes ninguna cuenta
                 <Link to="/register">
